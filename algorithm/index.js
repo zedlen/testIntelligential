52f43e9ea89e60f3ab015d5e4f939ad6b85758ec
Luis Melendez <luis.zdn@gmail.com>
2020-05-19 23:12:14 -0500

    remove trailing spaces

--- algorithm/index.js
+++ algorithm/index.js
@@ -1,15 +1,15 @@
 
 const caracol = ( array = [] ) => {
     var max_row = array.length - 1;
-    if (max_row < 0) {        
+    if (max_row < 0) {
         return [];
-    }    
-    if (max_row === 0) {        
+    }
+    if (max_row === 0) {
         return array[0];
-    }  
-    if (typeof array[0] !== 'object') {        
+    }
+    if (typeof array[0] !== 'object') {
         return array;
-    }  
+    }
     var max_col = array[0].length - 1;
     var keep_moving = true;
     var new_array = [];
@@ -17,58 +17,58 @@ const caracol = ( array = [] ) => {
     var move = 'rigth';
     var limit_x = 0;
     var limit_y = 0;
-    while ( keep_moving ) { 
-        if (x==0 && y == 0) {            
-            new_array.push(array[y][x]);  
-            array[y][x] = null                       
-        }          
+    while ( keep_moving ) {
+        if (x==0 && y == 0) {
+            new_array.push(array[y][x]);
+            array[y][x] = null;
+        }
         switch (move) {
             case 'rigth':
                 x++;
-                if ( x > max_col ) {                    
+                if ( x > max_col ) {
                     move = 'down';
                     max_col--;
                     x--;
                     y++;
                     limit_y++;
-                } 
+                }
                 break;
             case 'down':
                 y++;
                 if ( y > max_row ) { 
                     move = 'left';
                     max_row--;
-                    x--;   
-                    y--;                 
-                } 
+                    x--;
+                    y--;
+                }
                 break;
-            case 'left':                
+            case 'left':
                 x--;
                 if ( x < limit_x ) {
                     move = 'up';
                     x = limit_x;
                     limit_x++;
                     y--;
-                }                 
+                }
                 break;
-            case 'up':                        
-                y--;                
+            case 'up':
+                y--;
                 if ( y < limit_y ) {
                     move = 'rigth';
                     y = limit_y;
-                    limit_y++;                    
+                    limit_y++;
                     x++;
-                } 
+                }
                 break;
             default:
                 keep_moving = false;
                 break;
-        }            
-        new_array.push(array[y][x]); 
-        array[y][x] = null        
+        }
+        new_array.push(array[y][x]);
+        array[y][x] = null;
         if ( array.every(item=> item.every(item=>item === null )) ) {
-            keep_moving = false;         
-        }        
+            keep_moving = false;
+        }
     }
     return new_array;
 }
