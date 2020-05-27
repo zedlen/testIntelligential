ddd68ebad071a705f1c2cae9b5ef27c996d333a5
Luis Melendez <luis.zdn@gmail.com>
2020-05-26 23:07:11 -0500

    added validations

--- practiceCase/server/src/libs/middlewares.js
+++ practiceCase/server/src/libs/middlewares.js
@@ -1,6 +1,79 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import cors from 'cors'
+
+const premission ={
+  users:{
+    module: 'users',
+    methods:{
+      POST:'create',
+      GET:'list',
+      PUT:'update',
+      DELETE: 'delete'
+    }
+  },
+  userTypes:{
+    module: 'users',
+    methods:{
+      POST:'create',
+      GET:'list',
+      PUT:'update',
+      DELETE: 'delete'
+    }
+  },
+  profile:{
+    module: 'users',
+    methods:{
+      POST:'create',
+      GET:'list',
+      PUT:'update',
+      DELETE: 'delete'
+    }
+  },
+  profile:{
+    module: false
+  },
+  books:{
+    module: 'books',
+    methods:{
+      POST:'create',
+      GET:'list',
+      PUT:'update',
+      DELETE: 'delete'
+    }
+  },
+  borrow:{
+    module: 'books',
+    methods:{
+      POST:'borrow'
+    }
+  },
+  returnBook:{
+    module: 'books',
+    methods:{
+      POST:'update',
+    }
+  },
+  rejectLoan:{
+    module: 'books',
+    methods:{
+      PUT:'update',      
+    }
+  },
+  acceptLoan:{
+    module: 'books',
+    methods:{
+      PUT:'update',      
+    }
+  },
+  loans:{
+    module: 'books',
+    methods:{      
+      GET:'list',      
+    }
+  },
+}
+
 module.exports = app => {
 
   // Settings
@@ -16,15 +89,30 @@ module.exports = app => {
   });  
   const protectedRoutes = express.Router(); 
   protectedRoutes.use((req, res, next) => {
-      const token = req.headers['access-token'];
-  
+      const token = req.headers['access-token'];      
       if (token) {
-        jwt.verify(token, app.get('privateKey'), (err, decoded) => {      
+        jwt.verify(token, app.get('privateKey'), (err, decoded) => {                
           if (err) {
-            return res.json({ mensaje: 'Invalid Token' }).status(403);    
-          } else {
-            req.decoded = decoded;    
-            next();
+            return res.status(403).json({ mensaje: 'Invalid Token' });    
+          } else {  
+            decoded.permissions = JSON.parse(decoded.permissions)                    
+            const p = premission[req.path.split('/')[1]]
+            if(!p.module){
+              req.decoded = decoded;                
+              next();  
+            } else {
+              const userModulePermissions = decoded.permissions[p.module]
+              if (userModulePermissions === undefined) {
+                return res.status(401).json({ mensaje: 'No permission' });    
+              }              
+              const hasPermission = userModulePermissions.indexOf(p.methods[req.method]) > -1              
+              if (hasPermission) {
+                req.decoded = decoded;                
+                next();  
+              } else {
+                return res.status(401).json({ mensaje: 'No permission' });    
+              }
+            }
           }
         });
       } else {
